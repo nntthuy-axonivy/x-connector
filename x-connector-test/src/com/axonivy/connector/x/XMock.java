c7dbf7ce4e4d3059381ccb01ed48d6636a11add3
Tu Thanh Nguyen <138571181+tutn-axonivy@users.noreply.github.com>
2024-04-04 15:28:58 +0700

    MART-150 renaming repositories (#21)

---
+++ x-connector-test/src/com/axonivy/connector/x/XMock.java
@@ -0,0 +1,54 @@
+package com.axonivy.connector.x;
+
+import javax.annotation.security.PermitAll;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Path("xMock/2")
+@PermitAll
+@Hidden
+public class XMock {
+
+  @GET
+  @Path("users/by/username/{username}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response findUser(@PathParam("username") String username) {
+***REMOVED***return Response.status(200)
+***REMOVED******REMOVED******REMOVED***.entity("{\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED***\"data\": {\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED***\"id\": \"2829273353\",\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED***\"name\": \"Axon Ivy\",\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED***\"username\": \"" + username + "\"\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED***}\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "}")
+***REMOVED******REMOVED******REMOVED***.build();
+  }
+
+  @GET
+  @Path("tweets/search/recent")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response findTweets(@QueryParam("query") String query) {
+***REMOVED***return Response.status(200)
+***REMOVED******REMOVED******REMOVED***.entity("{\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED***\"data\": [\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED***{\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED******REMOVED***\"id\": \"1466242658364391426\",\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED******REMOVED***\"text\": \"this is a test tweet: " + query + ".\"\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED***}\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED***],\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED***\"meta\": {\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED***\"newest_id\": \"1466242658364391426\",\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED***\"oldest_id\": \"1466242658364391426\",\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED******REMOVED***\"result_count\": 1\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "***REMOVED***}\n"
+***REMOVED******REMOVED******REMOVED******REMOVED******REMOVED***+ "}")
+***REMOVED******REMOVED******REMOVED***.build();
+  }
+}
---
+++ x-connector-test/src_test/com/axonivy/connector/x/test/XTest.java
@@ -0,0 +1,50 @@
+package com.axonivy.connector.x.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.twitter.api.client.Tweet;
+import com.twitter.api.client.User;
+
+import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
+import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmElement;
+import ch.ivyteam.ivy.bpm.engine.client.element.BpmProcess;
+import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
+import ch.ivyteam.ivy.environment.AppFixture;
+
+@IvyProcessTest
+public class XTest {
+
+  private static final BpmProcess testee = BpmProcess.path("X");
+
+  @BeforeEach
+  void beforeEach(AppFixture fixture) {
+***REMOVED***//Disable OAuth feature for mock rest service
+***REMOVED***fixture.config("RestClients.X API (X API v2).Features", "ch.ivyteam.ivy.rest.client.mapper.JsonFeature");
+***REMOVED***fixture.var("X-connector.Url", "{ivy.app.baseurl}/api/xMock");
+  }
+
+  @Test
+  void findUser(BpmClient bpmClient) throws NoSuchFieldException {
+***REMOVED***BpmElement startable = testee.elementName("findUser(String)");
+***REMOVED***ExecutionResult result = bpmClient.start().subProcess(startable).execute("axonivy");
+***REMOVED***var user = (User) result.data().last().get("user");
+***REMOVED***assertThat(user.getName()).isEqualTo("Axon Ivy");
+***REMOVED***assertThat(user.getUsername()).isEqualTo("axonivy");
+  }
+
+  @Test
+  void findTweets(BpmClient bpmClient) throws NoSuchFieldException {
+***REMOVED***BpmElement startable = testee.elementName("findTweets(String)");
+***REMOVED***ExecutionResult result = bpmClient.start().subProcess(startable).execute("axonivy");
+***REMOVED***@SuppressWarnings("unchecked")
+***REMOVED***var tweets = (List<Tweet>) result.data().last().get("tweets");
+***REMOVED***assertThat(tweets).hasSize(1);
+***REMOVED***assertThat(tweets.get(0).getText()).isEqualTo("this is a test tweet: axonivy.");
+  }
+}
